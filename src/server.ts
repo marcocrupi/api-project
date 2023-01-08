ce62ffb40e69a9747ecbde8090f4f5e85cd3ebdf
marcocrupi <marcocrupi@hotmail.it>
2023-01-08 17:21:37 +0100

    configure our application with dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
+import "dotenv/config";
 import app from "./app";
 
-const port = 3000;
+const port = process.env.PORT;
 
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
