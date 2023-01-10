e51d7d19c9cdfab688c44cfd4cfeffe399cffa92
marcocrupi <marcocrupi@hotmail.it>
2023-01-10 09:55:02 +0100

    Integrate Prisma client with mocking

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import express from "express";
 import "express-async-errors";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 const app = express();
 
-app.get("/planets", (request, response) => {
-    response.json([{ name: "Mercury" }, { name: "Venus" }]);
+app.get("/planets", async (request, response) => {
+    const planets = await prisma.planet.findMany();
+
+    response.json(planets);
 });
 
 export default app;
--- src/server.test.ts
+++ src/server.test.ts
@@ -1,13 +1,39 @@
 import supertest from "supertest";
 import app from "./app";
 
+import { prismaMock } from "./lib/prisma/client.mock";
+
 const request = supertest(app);
 
 test("GET /planets", async () => {
+    const planets = [
+        {
+            id: 1,
+            name: "Mercury",
+            description: null,
+            diameter: 1234,
+            moons: 12,
+            CreatedAt: "2023-01-10T08:20:41.068Z",
+            updatedAt: "2023-01-10T08:20:55.985Z",
+        },
+        {
+            id: 2,
+            name: "Venus",
+            description: null,
+            diameter: 5678,
+            moons: 2,
+            CreatedAt: "2023-01-10T08:22:23.814Z",
+            updatedAt: "2023-01-10T08:21:51.139Z",
+        },
+    ];
+
+    //@ts-ignore
+    prismaMock.planet.findMany.mockResolvedValue(planets);
+
     const response = await request
         .get("/planets")
         .expect(200)
         .expect("Content-Type", /application\/json/);
 
-    expect(response.body).toEqual([{ name: "Mercury" }, { name: "Venus" }]);
+    expect(response.body).toEqual(planets);
 });
