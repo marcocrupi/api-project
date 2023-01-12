71da1ae15e870ef87971a1b9100fe4ab0b8f9b06
marcocrupi <marcocrupi@hotmail.it>
2023-01-12 08:47:06 +0100

    add validation for POST planet route

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,13 @@ import express from "express";
 import "express-async-errors";
 import prisma from "./lib/prisma/client";
 
+import {
+    validate,
+    validationErrorMiddleware,
+    planetSchema,
+    PlanetData,
+} from "./lib/validation";
+
 const app = express();
 
 app.use(express.json());
@@ -12,10 +19,16 @@ app.get("/planets", async (request, response) => {
     response.json(planets);
 });
 
-app.post("/planets", async (request, response) => {
-    const planet = request.body;
+app.post(
+    "/planets",
+    validate({ body: planetSchema }),
+    async (request, response) => {
+        const planet: PlanetData = request.body;
 
-    response.status(201).json(planet);
-});
+        response.status(201).json(planet);
+    }
+);
+
+app.use(validationErrorMiddleware);
 
 export default app;
--- src/server.test.ts
+++ src/server.test.ts
@@ -4,52 +4,74 @@ import app from "./app";
 import { prismaMock } from "./lib/prisma/client.mock";
 
 const request = supertest(app);
+describe("GET /planets", () => {
+    test("Valid request", async () => {
+        const planets = [
+            {
+                id: 1,
+                name: "Mercury",
+                description: null,
+                diameter: 1234,
+                moons: 12,
+                CreatedAt: "2023-01-10T08:20:41.068Z",
+                updatedAt: "2023-01-10T08:20:55.985Z",
+            },
+            {
+                id: 2,
+                name: "Venus",
+                description: null,
+                diameter: 5678,
+                moons: 2,
+                CreatedAt: "2023-01-10T08:22:23.814Z",
+                updatedAt: "2023-01-10T08:21:51.139Z",
+            },
+        ];
 
-test("GET /planets", async () => {
-    const planets = [
-        {
-            id: 1,
-            name: "Mercury",
-            description: null,
-            diameter: 1234,
-            moons: 12,
-            CreatedAt: "2023-01-10T08:20:41.068Z",
-            updatedAt: "2023-01-10T08:20:55.985Z",
-        },
-        {
-            id: 2,
-            name: "Venus",
-            description: null,
-            diameter: 5678,
-            moons: 2,
-            CreatedAt: "2023-01-10T08:22:23.814Z",
-            updatedAt: "2023-01-10T08:21:51.139Z",
-        },
-    ];
+        //@ts-ignore
+        prismaMock.planet.findMany.mockResolvedValue(planets);
 
-    //@ts-ignore
-    prismaMock.planet.findMany.mockResolvedValue(planets);
+        const response = await request
+            .get("/planets")
+            .expect(200)
+            .expect("Content-Type", /application\/json/);
 
-    const response = await request
-        .get("/planets")
-        .expect(200)
-        .expect("Content-Type", /application\/json/);
-
-    expect(response.body).toEqual(planets);
+        expect(response.body).toEqual(planets);
+    });
 });
 
-test("POST /planets", async () => {
-    const planet = {
-        name: "Mercury",
-        diameter: 1234,
-        moons: 12,
-    };
+describe("POST /planets", () => {
+    test("Valid request", async () => {
+        const planet = {
+            name: "Mercury",
+            diameter: 1234,
+            moons: 12,
+        };
 
-    const response = await request
-        .post("/planets")
-        .send(planet)
-        .expect(201)
-        .expect("Content-Type", /application\/json/);
+        const response = await request
+            .post("/planets")
+            .send(planet)
+            .expect(201)
+            .expect("Content-Type", /application\/json/);
 
-    expect(response.body).toEqual(planet);
+        expect(response.body).toEqual(planet);
+    });
+
+    test("Invalid request", async () => {
+        const planet = {
+            diameter: 1234,
+            moons: 12,
+        };
+
+        const response = await request
+            .post("/planets")
+            .send(planet)
+            .expect(422)
+            .expect("Content-Type", /application\/json/);
+
+        expect(response.body).toEqual({
+            errors: {
+                body: expect.any(Array),
+            },
+        });
+    });
 });
